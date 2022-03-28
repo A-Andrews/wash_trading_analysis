61f3cc53bf471e45f7bfd2afe3cb39f8e08b6d84
Austin Andrews <austinandrews6@gmail.com>
2022-03-28 21:51:48 +0100

    Add function to get

--- utility-files/data_gatherer.py
+++ utility-files/data_gatherer.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import csv
+import json
 
 #{
 #  "operation": "GetAxieDetail",
@@ -21,15 +23,43 @@ axie_url = "https://graphql-gateway.axieinfinity.com/graphql"
 
 
 
-def get_axie_attributes(id: int):
-    axie_id = str(id)
+def get_axie_attributes(ax_id: int):
+    axie_id = str(ax_id)
     payload = {
                 "operation": "GetAxieDetail",
-                "variables": {"axieId": "1000"},
+                "variables": {"axieId": ax_id},
                 "query": "query GetAxieDetail($axieId: ID!) {axie(axieId: $axieId) {...AxieDetail}}fragment AxieDetail on Axie {id class genes class breedCount level parts { ...AxiePart} stats { ...AxieStats}} fragment AxiePart on AxiePart {class type specialGenes abilities {...AxieCardAbility}} fragment AxieCardAbility on AxieCardAbility {id name attack defense energy} fragment AxieStats on AxieStats {hp speed skill morale}"}
     headers = {"content-type": "application/json"}
 
     response = requests.request("POST", axie_url, json=payload, headers=headers)
-    return response.json
+    return response.json()
 
-print(get_axie_attributes(1000))
+def write_axie_to_row(axie):
+    data = axie["data"]["axie"]
+    ax_id, ax_class, genes, breedCount, level = data["id"], data["class"], data["genes"], data["breedCount"], data["level"]
+    stats = data["stats"]
+    hp, speed, skill, morale = stats["hp"], stats["speed"], stats["skill"], stats["morale"]
+    parts = data["parts"]
+    eyes, ears, back, mouth, horn, tail = parts[0]["class"], parts[1]["class"], parts[2]["class"], parts[3]["class"], parts[4]["class"], parts[5]["class"]
+    ability1, ability2, ability3, ability4 = parts[2]["abilities"][0]["name"], parts[3]["abilities"][0]["name"], parts[4]["abilities"][0]["name"], parts[5]["abilities"][0]["name"]
+
+    return [ax_id,ax_class,genes,breedCount,level,hp,speed,skill,morale,eyes,ears,back,mouth,horn,tail,ability1,ability2,ability3,ability4]
+
+
+def get_csv_axie():
+
+    f = open('attribute_files/axie_attributes.csv', 'w')
+
+    writer = csv.writer(f)
+
+    writer.writerow(["id","class","genes","breedCount","level","hp","speed","skill","morale","eyes","ears","back","mouth","horn","tail","ability1","ability2","ability3","ability4"])
+
+    for i in range(1000, 11000):
+        data = get_axie_attributes(i)
+        row = write_axie_to_row(data)
+        writer.writerow(row)
+
+    f.close()
+        
+
+get_csv_axie()
