8044d463057bdf7cc484ca695aecfe3e7b4a328e
Austin Andrews <austinandrews6@gmail.com>
2022-03-30 13:15:32 +0100

    Implement plotting and saving of FAMD

--- README.md
+++ README.md
@@ -18,3 +18,11 @@ Leave with:
 > deactivate
 
 ## Usage
+In order to get CSV files:
+> python3 data_gatherer.py id_start id_end series
+Where:
+- id_start is the id to start gethering from
+- id_end is the id to stop gathering at
+- series is the name to determine which series to gather
+
+At some point these will be handled by a bash file.
--- plotting_files/cluster_analysis.py
+++ plotting_files/cluster_analysis.py
@@ -1,3 +1,31 @@
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
 from distance_metrics import normalise_attributes, get_attribute_data
+from light_famd import FAMD
 
-print(normalise_attributes(get_attribute_data("attribute_files/axie_attributes.csv")).dtypes)
+def plot_pca_explained(explained, series):
+    plt.title(f'Proportion of explained variance of principal components of {series} for d = {len(explained)}')
+    plt.ylabel('Proportion of explained variance')
+    plt.xlabel('d')
+    plt.xticks(range(1,len(explained)+1))
+    plt.bar(range(1,len(explained)+1), explained / explained.sum())
+    plt.savefig(f'graphs/pca_explained_{series}_d{len(explained)}.png')
+
+
+def main(argv):
+    series = argv[0]
+    clustering_type = argv[1]
+    data = get_attribute_data(f"attribute_files/{series}_attributes.csv")
+    norm_data = normalise_attributes(data)
+    norm_data.drop('id', 1, inplace=True)
+    if clustering_type == "famd":
+        famd = FAMD(n_components=2)
+        famd.fit(norm_data)
+        explained_var = famd.explained_variance_
+        explained_var_ratio = famd.explained_variance_ratio_
+        norm_data_famd = famd.transform(norm_data)
+        plot_pca_explained(explained_var, series)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
