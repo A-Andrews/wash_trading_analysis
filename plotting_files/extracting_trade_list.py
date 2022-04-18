9d19445f5d4e2893006033196b5c07075c218687
Austin Andrews <austinandrews6@gmail.com>
2022-04-18 17:08:30 +0100

    Implement data retrieval for networking

--- plotting_files/extracting_trade_list.py
+++ plotting_files/extracting_trade_list.py
@@ -1,12 +1,27 @@
 import pandas as pd
+import numpy as np
 
 def get_opensea_trade_data(series):
     data = pd.read_csv(f"transaction_files/{series}_transfers.csv")
     ids = data[['id']].copy()
     ids = ids['id'].unique()
 
-    # Get lists of rows associated with particular time stamp
-    for i in ids:
-        return 0
+    return data, ids
 
-get_opensea_trade_data('BAYC')
+    
+def get_opensea_addresses_times(data, i):
+    rows = data.loc[data["id"] == i]
+    wallets = rows[['buyer_address']].to_numpy()
+    times = rows[['time']].to_numpy()
+
+    return wallets, times
+        
+def get_all_address_time_pairs(data, ids):
+    address_list = []
+    times_list = []
+    for i in ids:
+        wallets, times = get_opensea_addresses_times(data, i)
+        address_list.append(wallets)
+        times_list.append(times)
+
+    return address_list, times_list
