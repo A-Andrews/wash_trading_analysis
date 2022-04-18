9a62398de499118c6d9563d4fc61e381c682241b
Austin Andrews <austinandrews6@gmail.com>
2022-04-18 20:03:11 +0100

    Implement retrieval of to and from addresses

--- plotting_files/extracting_trade_list.py
+++ plotting_files/extracting_trade_list.py
@@ -5,9 +5,11 @@ def get_opensea_trade_data(series):
     data = pd.read_csv(f"transaction_files/{series}_transfers.csv")
     ids = data[['id']].copy()
     ids = ids['id'].unique()
+    addresses = data[['buyer_address']].copy()
+    addresses = addresses['buyer_address'].unique()
+    print(len(addresses))
 
-    return data, ids
-
+    return data, ids, addresses
     
 def get_opensea_addresses_times(data, i):
     rows = data.loc[data["id"] == i]
@@ -25,3 +27,14 @@ def get_all_address_time_pairs(data, ids):
         times_list.append(times)
 
     return address_list, times_list
+
+def get_to_from_addresses(data, address):
+    to_rows = data.loc[data['seller_address'] == address]
+    to_addresses = to_rows[['buyer_address']].to_numpy()
+
+    from_rows = data.loc[data['buyer_address'] == address]
+    from_addresses = from_rows[['seller_address']].to_numpy()
+
+    return to_addresses, from_addresses
+
+
--- plotting_files/trade_network.py
+++ plotting_files/trade_network.py
@@ -1,10 +1,25 @@
 import sys
+import extracting_trade_list
+from plotting_files.extracting_trade_list import get_opensea_trade_data
+
+def create_adjacency_matrix():
+    return 0
+
+def list_for_time_period():
+    return 0
+
+def create_graph_for_one(addresses, i, time):
+
+    return 0
 
 def main(argv):
     series = argv[0]
     network_type = argv[1]
     time_range_start = argv[2]
     time_range_end = argv[3]
+
+    if series == 'BAYC':
+        data, ids, addresses = get_opensea_trade_data(series)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
