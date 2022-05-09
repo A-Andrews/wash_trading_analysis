8180e5607db43bbc53269374c7f8414abc3eff89
Austin Andrews <austinandrews6@gmail.com>
2022-05-09 18:24:21 +0100

    Implement Chord Diagram Pairs

--- plotting_files/chord_diagrams.py
+++ plotting_files/chord_diagrams.py
@@ -17,6 +17,13 @@ def simple_common_chord(data, common_number, amount, series):
     adj_mat = create_adjacency_matrix(top_pairs)
     plot_chord(adj_mat)
 
+def simple_common_pairs_chord(data, common_number, amount, series):
+    pairs = get_common_pairs(data, common_number)
+    top_pairs = get_topx_common(pairs, amount)
+    top_pairs = get_node_pairs_from_pairs(top_pairs)
+    adj_mat = create_adjacency_matrix(top_pairs)
+    plot_chord(adj_mat)
+
 def main(argv):
     series = argv[0]
     network_type = argv[1]
@@ -34,6 +41,8 @@ def main(argv):
             data = remove_out_of_time(data, time_range_start, time_range_end)
     if network_type == 'simple_common_chord':
         simple_common_chord(data, common_number, amount, series)
+    elif network_type == 'simple_common_pairs_chord':
+        simple_common_pairs_chord(data, common_number, amount, series)
         
 
 if __name__ == "__main__":
