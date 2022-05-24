3ba8d46c31f7b34ad810aa5b1bdc8c5221ca90fd
Austin Andrews <austinandrews6@gmail.com>
2022-05-24 18:23:25 +0100

    Add colour based labelling rather than size based

--- plotting_files/trade_network.py
+++ plotting_files/trade_network.py
@@ -1,7 +1,9 @@
 import sys
 from extracting_trade_list import *
 import networkx as nx
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 def list_for_time_period():
     return 0
@@ -16,12 +18,23 @@ def create_adjacency_graph(data, adj_mat, weights):
     g = nx.from_pandas_adjacency(adj_mat)
     list_degree=list(g.degree())
     pos = nx.spring_layout(g, seed=4)
-    nodes, degree = map(list, zip(*list_degree))
-    nodes_amounts = get_owned_nums(data, replace_mixed_names_addresses(data, nodes))
-    nx.draw_networkx(g, pos = pos, font_size=6, nodelist=nodes, node_size=[(v * 0.7)+1 for v in nodes_amounts], arrowstyle='-|>')
+    nodes_list, degree = map(list, zip(*list_degree))
+    nodes_amounts = get_owned_nums(data, replace_mixed_names_addresses(data, nodes_list))
+    node_size = [(v * 0.3) for v in nodes_amounts]
 
-    for i, e in enumerate(g.edges):
-        nx.draw_networkx_edges(g, pos, edgelist=[e], width = weights[i]*0.1)
+    nodes = nx.draw_networkx_nodes(g, pos = pos, nodelist=nodes_list, node_size = node_size, alpha = 0.6)
+    labels = nx.draw_networkx_labels(g, pos, font_size = 6, alpha = 0.7)
+
+    weights_rgb = [plt.cm.plasma(i / max(weights)) for i in weights]
+    cmap = plt.cm.plasma
+
+    #edges = nx.draw_networkx_edges(g, pos, edge_color = weights_rgb, edge_cmap = cmap, nodelist = nodes_list, node_size = node_size, width = 2, arrowsize=4, arrows = True)
+    edges = nx.draw_networkx_edges(g, pos, edge_color = weights_rgb, edge_cmap = cmap, nodelist = nodes_list, node_size = node_size, width = 2, arrowsize=4, arrows = True)
+
+    pc = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin = min(weights), vmax=max(weights)))
+    plt.colorbar(pc)
+
+    return nodes, edges, labels
 
 def common_singles_network(data, common_number, series):
     common_addresses = get_common_addresses(data, common_number)
@@ -30,7 +43,6 @@ def common_singles_network(data, common_number, series):
     pairs = replace_pairs_names(data, pairs)
     adj_mat = create_adjacency_matrix(pairs)
     create_adjacency_graph(data, adj_mat, weights_p)
-    plt.show()
 
 def common_pairs_network(data, common_number, series):
     pairs = get_common_pairs(data, common_number)
@@ -39,7 +51,6 @@ def common_pairs_network(data, common_number, series):
     pairs = replace_pairs_names(data, pairs)
     adj_mat = create_adjacency_matrix(pairs)
     create_adjacency_graph(data, adj_mat, weights_p)
-    plt.show()
 
 def common_sequences_network(data, ids, series, min_length = 1, min_occurances = 1):
     sequences = find_common_sequences(data, ids, min_length, min_occurances)
@@ -48,7 +59,6 @@ def common_sequences_network(data, ids, series, min_length = 1, min_occurances =
     pairs = replace_pairs_names(data, pairs)
     adj_mat = create_adjacency_matrix(pairs)
     create_adjacency_graph(data, adj_mat, weights_p)
-    plt.show()
 
 def common_associations_network(data, ids, series, min_length = 1, min_occurances = 1):
     sequences = find_associated_addresses(data, ids, min_length, min_occurances)
@@ -57,7 +67,6 @@ def common_associations_network(data, ids, series, min_length = 1, min_occurance
     pairs = replace_pairs_names(data, pairs)
     adj_mat = create_adjacency_matrix(pairs)
     create_adjacency_graph(data, adj_mat, weights_p)
-    plt.show()
 
 def simple_loops_network(data, ids, series, min_length = 1, min_occurances = 1):
     sequences = find_common_sequences(data, ids, min_length, min_occurances)
@@ -67,7 +76,10 @@ def simple_loops_network(data, ids, series, min_length = 1, min_occurances = 1):
     pairs = replace_pairs_names(data, pairs)
     adj_mat = create_adjacency_matrix(pairs)
     create_adjacency_graph(data, adj_mat, weights_p)
-    plt.show()
+
+def update(time):
+
+    edges
 
 
 def main(argv):
@@ -95,6 +107,8 @@ def main(argv):
         common_associations_network(data, ids, series, common_number, common_number)
     if network_type == 'simple_loops':
         simple_loops_network(data, ids, series, common_number, common_number)
+
+    plt.show()
         
 
 
